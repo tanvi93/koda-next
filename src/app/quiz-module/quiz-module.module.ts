97744a0381b2508275e0090fefc9390ce308aad1
Amit_R_Singh <amitsingh@helixtechsolutions.com>
2018-06-26 10:47:55 +0530

    unnecessary radio component deleted

--- src/app/quiz-module/quiz-module.module.ts
+++ src/app/quiz-module/quiz-module.module.ts
@@ -6,13 +6,12 @@ import { SharedModuleModule } from './../shared-module/shared-module.module';
 import { QuizModuleRoutingModule } from './quiz-module-routing.module';
 import { QuizWrapperComponent } from './quiz-wrapper/quiz-wrapper.component';
 import { CheckboxComponentComponent } from './checkbox-component/checkbox-component.component';
-import { RadioComponentComponent } from './radio-component/radio-component.component';
 @NgModule({
   imports: [
     CommonModule,
     SharedModuleModule,
     QuizModuleRoutingModule
   ],
-  declarations: [QuizWrapperComponent, CheckboxComponentComponent, RadioComponentComponent]
+  declarations: [QuizWrapperComponent, CheckboxComponentComponent]
 })
 export class QuizModuleModule { }
