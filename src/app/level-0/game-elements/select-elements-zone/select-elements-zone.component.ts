6c558eeff48a9fe32656c1a3c71810eb2ac3156d
tanvi93 <tanviraul23@gmail.com>
2018-06-27 17:23:31 +0530

    working on validation refactoring

--- src/app/level-0/game-elements/select-elements-zone/select-elements-zone.component.ts
+++ src/app/level-0/game-elements/select-elements-zone/select-elements-zone.component.ts
@@ -7,10 +7,49 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SelectElementsZoneComponent implements OnInit {
   @Input() gameInfo;
-  
-  constructor() { }
+  @Input() presentQuestionData;
+  private questionCount: number;
+  private charClicked: any;
+  private showRules: Boolean;
+  // private noCorrectOption: Boolean;
+
+  constructor() {
+    this.questionCount = 0;
+    this.charClicked = [];
+    this.showRules = false;
+    // this.noCorrectOption = true;
+  }
 
   ngOnInit() {
+    this.presentQuestionData = this.gameInfo.validation[this.questionCount];
+    console.log(this.presentQuestionData);
+  }
+
+  elementClicked(event) {
+    for (let i = 0; i < this.presentQuestionData.options.length; i++) {
+
+      if (this.presentQuestionData.options[i].option === event.target.id) {
+        if (this.presentQuestionData.options[i].isCorrect) {
+          console.log(this.presentQuestionData.options[i].correctMsg);
+          this.presentQuestionData.options[i].isCorrect = false;
+          this.charClicked.push(event.target.id);
+          console.log(this.charClicked);
+          if (this.charClicked.length >= 3) {
+            console.log("inside" ,this.charClicked);
+            this.questionCount++;
+            setTimeout(() => {
+              this.presentQuestionData = this.gameInfo.validation[this.questionCount];
+              if (this.charClicked.length > 3) {
+                this.showRules = true;
+              }
+            }, 20);
+          }
+          
+        } else {
+          console.log(this.presentQuestionData.options[i].errorMsg);
+        }
+      }
+    }
   }
 
 }
