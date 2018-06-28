d694f2b67d2c8cfe4ee244d8eddd25c3a16e5925
tanvi93 <tanviraul23@gmail.com>
2018-06-28 17:25:53 +0530

    working on algo drag drop

--- src/app/level-0/algo-dragn-drop/algo-dragn-drop.component.ts
+++ src/app/level-0/algo-dragn-drop/algo-dragn-drop.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { algoStep } from '../../data/dragAndDrop';
 
 @Component({
   selector: 'app-algo-dragn-drop',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./algo-dragn-drop.component.scss']
 })
 export class AlgoDragnDropComponent implements OnInit {
+  private algorithmObj: any;
+  private pageId: String;
+  private loading: Boolean;
+  private imageCount;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.loading = true;
+    this.imageCount = 0;
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.pageId = params.pageId;
+      this.algorithmObj = algoStep[`${this.pageId}`];
+    });
+    const imageLoad = () => {
+      this.imageCount++;
+      if (this.algorithmObj.steps.length === this.imageCount) {
+        this.loading = false;
+      }
+    };
+
+    for (let i = 0; i < this.algorithmObj.steps.length; i++) {
+      const image = new Image();
+      image.onload = imageLoad;
+      image.src = this.algorithmObj.steps[i].icon;
+    }
   }
 
 }
---
+++ src/app/level-0/algo-dragn-drop/drag-zone/drag-zone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drag-zone',
+  templateUrl: './drag-zone.component.html',
+  styleUrls: ['./drag-zone.component.scss']
+})
+export class DragZoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
