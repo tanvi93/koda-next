5c681c0a4324c6f5c2e9f3b13d3b5c1a3dc80339
Hemangi Amol Pawar <hemangip@Nextmumbai>
2018-06-29 18:29:43 +0530

    fox and gerape activity storyline

--- src/app/data/storyline.ts
+++ src/app/data/storyline.ts
@@ -1,5 +1,9 @@
 export const stories = {
-    magical_cave: {
+    fox_n_grapes: {
+        backgroundImgs: [
+            './assets/images/activities/fox_and_grapes/fox_activity_bg.jpg'
+        ]
+    }, magical_cave: {
         backgroundImgs: [
             './assets/images/activities/magical_cave/storyline.jpg'
         ]
