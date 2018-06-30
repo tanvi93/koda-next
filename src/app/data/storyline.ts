c05781c803f44f0f81ec4da48ad5e01073facf2f
Yash Kochar <yashkochar@nexteducation.in>
2018-06-30 18:08:06 +0530

    images deleted

--- src/app/data/storyline.ts
+++ src/app/data/storyline.ts
@@ -1,7 +1,7 @@
 export const stories = {
     fox_n_grapes: {
         backgroundImgs: [
-            './assets/images/activities/fox_and_grapes/fox_activity_bg.jpg'
+            'http://dqfevutdn6sxd.cloudfront.net/activities/fox_and_grapes/fox_activity_bg.jpg'
         ]
     },
     treasure_hunt: {
@@ -48,12 +48,12 @@ export const stories = {
         ]
     }, identical_twins: {
         backgroundImgs: [
-            './assets/images/activities/identical_twins/step1.png',
-            './assets/images/activities/identical_twins/step2.png',
-            './assets/images/activities/identical_twins/step3.png',
-            './assets/images/activities/identical_twins/step4.png',
-            './assets/images/activities/identical_twins/step5.png',
-            './assets/images/activities/identical_twins/step6.png'
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step1.png',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step2.png',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step3.png',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step4.png',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step5.png',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/identical_twins/step6.png'
         ]
     }, eclipse: {
         backgroundImgs: [
@@ -63,6 +63,11 @@ export const stories = {
         backgroundImgs: [
             'http://dqfevutdn6sxd.cloudfront.net/activities/fire_extinguisher/fire-ext-step1.jpg',
             'http://dqfevutdn6sxd.cloudfront.net/activities/fire_extinguisher/fire-ext-step2.jpg'
+        ]
+    }, lambent_app: {
+        backgroundImgs: [
+            'http://dqfevutdn6sxd.cloudfront.net/activities/lambent_app/step1.jpg',
+            'http://dqfevutdn6sxd.cloudfront.net/activities/lambent_app/step2.jpg'
         ]
     }
 }
