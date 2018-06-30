9f25889d388c4bb1179c04f03121d195a7edeacb
Hemangi Amol Pawar <hemangip@Nextmumbai>
2018-06-30 12:04:21 +0530

    treasure-hunt-storyline

--- src/app/data/storyline.ts
+++ src/app/data/storyline.ts
@@ -4,6 +4,11 @@ export const stories = {
             './assets/images/activities/fox_and_grapes/fox_activity_bg.jpg'
         ]
     },
+    treasure_hunt: {
+        backgroundImgs: [
+            './assets/images/activities/treasure_hunt/bg_storyline.jpg'
+        ]
+    },
     magical_cave: {
         backgroundImgs: [
             './assets/images/activities/magical_cave/storyline.jpg'
