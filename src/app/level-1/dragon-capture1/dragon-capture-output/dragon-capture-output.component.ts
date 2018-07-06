254dd95b71137755d3ba992c8810b7b1b8fcccc2
Amit_R_Singh <amitsingh@helixtechsolutions.com>
2018-07-06 17:33:01 +0530

    final validation along with documentation completed

--- src/app/level-1/dragon-capture1/dragon-capture-output/dragon-capture-output.component.ts
+++ src/app/level-1/dragon-capture1/dragon-capture-output/dragon-capture-output.component.ts
@@ -1,18 +1,184 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { SuccessModalComponent } from './../../../shared-module/success-modal/success-modal.component';
+
 
 @Component({
   selector: 'app-dragon-capture-output',
   templateUrl: './dragon-capture-output.component.html',
   styleUrls: ['./dragon-capture-output.component.scss']
 })
+  
+  /**
+   * @name DragonCaptureOutputComponent<app-dragon-capture-output>
+   * @description This component will deal dragon capture activity1 in which the user have to grab have to find required object inorder to tame the dragon.
+   * @param contentData its holds entire detail of the page which is passed as an input from its parent component.
+   * @constructor mat dialog is injected which will be triggered when all the task has been performed
+   */
+
+  /**
+   * @method wrongAttempt
+   * @memberOf DragonCaptureOutputComponent
+   * @param ev This varaible contain the environment objhect through which appropriate msg at clicked position is been displayed.
+   * @description this is an event based method which get triggered whenever user click at wrong coordinate result in providing error msg to user.
+   */
+
+  /**
+   * @method successfulAttempt
+   * @memberOf DragonCaptureOutputComponent
+   * @param ev This varaible contain the environment objhect through which appropriate msg at clicked position is been displayed.
+   * @description this is an event based method which get triggered whenever user find correct coordinate which will help in capturing dragon and provide appropriate msg accordingly.
+   */
+
+  /**
+   * @method successCondition
+   * @memberOf DragonCaptureOutputComponent
+   * @param count This varaible contain count about how much object have been found.
+   * @description this method check whether appropriate number of object have been found and its reached amount its waiting then it will trigger success popup.
+   */
 export class DragonCaptureOutputComponent implements OnInit {
   @Input() contentData;
   @Output() dragonObject = new EventEmitter<number>();
+  @Output() inputFlagStatus = new EventEmitter<number>();
+  dialogRef: MatDialogRef<SuccessModalComponent>;
+
   private backgroundImage: string;
-  constructor() { }
+  private coordVisibility: boolean;
+  private gemOpacity: number;
+  private gridEnabled: boolean;
+  private message: string;
+  private hideMsg: boolean;
+  private articleTopShift: string[];
+  private articleHidden: boolean[];
+  private clickableContentFlag: boolean[];
+  msgPos = { 'msgLeft': null, 'msgTop': null, 'msgTailTop': null, 'msgTailLeft': null };
+ 
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-    this.backgroundImage = this.contentData.backgroundImage[1];
+    this.backgroundImage = this.contentData.backgroundImage[0];
+    this.hideMsg = true;
+    this.gemOpacity = 0;
+    this.articleHidden = [true, true, true];
+    this.clickableContentFlag = [false, true, true];
+    this.gridEnabled = true;
+    this.articleTopShift = ['60', '68', '72'];
+    this.coordVisibility = true;
+    this.msgPos = this.contentData.initialMsgPos;
+    this.message = this.contentData.errorMsg;
   }
 
+  wrongAttempt = (ev) =>{
+    this.message = this.contentData.errorMsg;
+    if (ev.layerX / ev.target.clientWidth > 0.8 && ev.layerY / ev.target.clientHeight < 0.1) {
+      this.msgPos.msgTailLeft = '76%';
+      this.msgPos.msgLeft = (ev.layerX * 0.75) + 'px';
+      this.msgPos.msgTop = (ev.layerY) + 'px';
+    } else if (ev.layerX / ev.target.clientWidth > 0.8 && ev.layerY / ev.target.clientHeight > 0.9) {
+      this.msgPos.msgTailLeft = '76%';
+      this.msgPos.msgLeft = (ev.layerX * 0.75) + 'px';
+      this.msgPos.msgTop = '90%';
+    } else if (ev.layerY / ev.target.clientHeight < 0.1) {
+      this.msgPos.msgTop = (ev.layerY) + 'px';
+      this.msgPos.msgLeft = ev.layerX + 'px';
+      this.msgPos.msgTailLeft = '7%';
+    } else if (ev.layerX / ev.target.clientWidth > 0.8) {
+      this.msgPos.msgTop = (ev.layerY - 20) + 'px';
+      this.msgPos.msgLeft = (ev.layerX * 0.73) + 'px';
+      this.msgPos.msgTailLeft = '76%';
+    } else if (ev.layerY / ev.target.clientHeight > 0.9) {
+      this.msgPos.msgTop = '90%';
+      this.msgPos.msgLeft = ev.layerX + 'px';
+      this.msgPos.msgTailLeft = '7%';
+    } else {
+      this.msgPos.msgTop = (ev.layerY - 20) + 'px';
+      this.msgPos.msgLeft = ev.layerX + 'px';
+      this.msgPos.msgTailLeft = '7%';
+    }
+    this.msgPos.msgTailTop = this.contentData.initialMsgPos.msgTailTop;
+    setTimeout(() => {
+      this.hideMsg = false;
+      this.gridEnabled = false;
+    }, 100);
+    setTimeout(() => {
+      this.hideMsg = true;
+      this.gridEnabled = true;
+    }, 3000);
+  }
+
+  successfulAttempt = (ev) => {
+    this.gridEnabled = false;
+    setTimeout(() => {
+      this.hideMsg = false;
+    }, 500);
+    ev.target.style.display = 'none';
+    setTimeout(() => {
+      this.articleTopShift[Number(ev.target.id)] = '0';
+    }, 100);
+    this.msgPos.msgTop = (this.contentData.yGridCoord.indexOf(this.contentData.coordinates[Number(ev.target.id)].y) + 0.5) * this.contentData.yGridPerUnitScale + '%';
+    this.msgPos.msgLeft = (this.contentData.xGridCoord.indexOf(this.contentData.coordinates[Number(ev.target.id)].x) + 1.5) * this.contentData.xGridPerUnitScale + '%';
+    this.msgPos.msgTailLeft = this.contentData.initialMsgPos.msgTailLeft;
+    this.msgPos.msgTailTop = this.contentData.initialMsgPos.msgTailTop;
+
+    switch (Number(ev.target.id) + 1) {
+
+      case 1: {
+        this.articleHidden[Number(ev.target.id)] = false;
+        this.message = this.contentData.coordinates[Number(ev.target.id)].message;
+        this.dragonObject.emit(Number(ev.target.id));
+        setTimeout(() => {
+          this.articleHidden[Number(ev.target.id)] = true;
+          this.clickableContentFlag[1] = false;
+          this.gridEnabled = true;
+          this.hideMsg = true;
+          this.coordVisibility = false;
+          this.backgroundImage = this.contentData.backgroundImage[1];
+          this.inputFlagStatus.emit(Number(ev.target.id));
+        }, 3500);
+        break;
+      }
+      case 2: {
+        this.articleHidden[Number(ev.target.id)] = false;
+        this.message = this.contentData.coordinates[Number(ev.target.id)].message;
+        this.dragonObject.emit(Number(ev.target.id));
+        setTimeout(() => {
+          this.gridEnabled = true;
+          this.clickableContentFlag[2] = false;
+          this.hideMsg = true;
+          this.articleHidden[Number(ev.target.id)] = true;
+          this.inputFlagStatus.emit(Number(ev.target.id));
+        }, 5500);
+        break;
+      }
+      case 3: {
+        this.articleHidden[Number(ev.target.id)] = false;
+        this.message = this.contentData.coordinates[Number(ev.target.id)].message;
+        this.dragonObject.emit(Number(ev.target.id));
+        setTimeout(() => {
+          this.gridEnabled = true;
+          this.successCondition();
+          this.hideMsg = true;
+          this.articleHidden[Number(ev.target.id)] = true;
+          this.inputFlagStatus.emit(Number(ev.target.id));
+        }, 3000);
+        break;
+      }
+      default: ;
+    }
+
+  }
+
+  successCondition = () => {
+      this.gridEnabled = false;
+      this.gemOpacity = 1;
+      setTimeout(() => {
+        this.dialogRef = this.dialog.open(SuccessModalComponent, {
+          hasBackdrop: true,
+          disableClose: true,
+          panelClass: 'app-full-bleed-dialog'
+        });
+        this.dialogRef.componentInstance.modalData = this.contentData;
+      }, 2000);
+  }
 }
+
--- src/app/level-1/dragon-capture1/dragon-capture1.component.ts
+++ src/app/level-1/dragon-capture1/dragon-capture1.component.ts
@@ -1,25 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { dragonCaptureContent } from './../../data/dragonCaptureActivities'
+import { AudioService } from './../../shared-services/audio.service'
 
 @Component({
   selector: 'app-dragon-capture1',
   templateUrl: './dragon-capture1.component.html',
   styleUrls: ['./dragon-capture1.component.scss']
 })
+  
+/**
+   * @name DragonCapture1Component<app-dragon-capture1>
+   * @description This component will deal dragon capture activity1 in which the user have to grab have to find required object inorder to tame the dragon.
+   * @param contentData its holds entire detail of the page which is passed as an input to its child component.
+   * @constructor cricket audio is set so that it will keep on running through out the process.
+   */
+
+  /**
+   * @method displayFlag
+   * @memberOf DragonCapture1Component
+   * @param $event This varaible contain the index through which the correct flag is been set visible.
+   * @description this is an event based method which get triggered whenever user find correct coordinate which will help in capturing dragon.
+   */
+
+  /**
+   * @method updateTodoList
+   * @memberOf DragonCapture1Component
+   * @param $event This varaible contain index value which will help in enabling next coordinate.
+   * @description this is an event based method which get triggered whenever user find correct coordinate which will help in capturing dragon.
+   */
 export class DragonCapture1Component implements OnInit {
   private contentData = dragonCaptureContent;
   private question: string;
   private listStatus: boolean[];
   private checkStatus: boolean[];
-  constructor() { }
+  private cricketAudio: string[];
+  constructor(private audio: AudioService) {
+    this.cricketAudio = ['assets/audio/dragon/crickets.wav'];
+  }
 
   ngOnInit() {
     this.question = this.contentData.questionArray[0];
     this.listStatus = [false, true, true];
     this.checkStatus = [true, true, true];
+    this.audio.loader(this.cricketAudio);
+    this.audio.playForever(0);
   }
 
+  displayFlag = ($event) => {
+    this.listStatus[$event + 1] = false;
+    if ($event < this.contentData.questionArray.length -1) {
+      this.question = this.contentData.questionArray[$event + 1]
+    }
+  }
   updateTodoList = ($event) => {
-    console.log($event);
+    this.checkStatus[$event] = false;
   }
 }
