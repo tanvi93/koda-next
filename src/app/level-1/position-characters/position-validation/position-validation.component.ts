6bced6d8d24a70757ec7eb0ce21e2197313d9c77
Amit_R_Singh <amitsingh@helixtechsolutions.com>
2018-07-10 11:47:28 +0530

    documentation of position character completed

--- src/app/level-1/position-characters/position-validation/position-validation.component.ts
+++ src/app/level-1/position-characters/position-validation/position-validation.component.ts
@@ -6,6 +6,20 @@ import { selectCharacterContent } from './../../../data/selectCharacterContent';
   templateUrl: './position-validation.component.html',
   styleUrls: ['./position-validation.component.scss']
 })
+  
+  /**
+   * @name PositionValidationComponent<app-position-validation>
+   * @description This component will deal positioning of choosen character which user have choosen in select character activity
+   * @param contentData its holds entire detail of the page which is passed as an input to its child component.
+   * @constructor intiate all parameter related to character and its indicator.
+   */
+
+
+  /**
+   * @method hideError
+   * @memberOf PositionValidationComponent
+   * @description this method help in hiding the errror msg whenever user changes position of character which is then conveyed by his parent.
+   */
 export class PositionValidationComponent implements OnInit, OnChanges {
   @Input() contentData;
   @Input() errorFlag;
